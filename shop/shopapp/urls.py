f8f4bf766974cee03a62a6242e5a9955449020b0
Marya373 <z89262554967@gmail.com>
2024-01-23 16:34:54 +0300

    Иван - лучший!

--- shop/shopapp/urls.py
+++ shop/shopapp/urls.py
@@ -4,7 +4,7 @@ from shopapp.views import get_all_list_order, get_list_products_by_customer, hel
 app_name = 'shopapp'
 
 urlpatterns = [
-    path('get_all_list_order/', get_all_list_order, name='get_all_list_order'),
-    path('get_list_products_by_customer/', get_list_products_by_customer, name='get_list_products_by_customer'),
+    path('get_all_list_order/<name_client>/', get_all_list_order, name='get_all_list_order'),
+    path('get_list_products_by_customer/<name_client>/', get_list_products_by_customer, name='get_list_products_by_customer'),
     path('hello/', hello, name='hello'),
 ]
